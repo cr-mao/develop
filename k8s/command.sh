a9460c9d2d543497bdf33ef161ad32cfd3192810
cr-mao <crmao@qq.com>
2022-11-25 02:07:08 +0800

    k8s job cronjob

--- k8s/command.sh
+++ k8s/command.sh
@@ -1,5 +1,48 @@
 #!/bin/bash
 
- kubectl get pod -o wide -A
+# 放到~/.bashrc下 kubectl自动命令补全
+source <(kubectl completion bash)
+
+kubectl run ngx --image=nginx:alpine
+
+kubectl get pod -o wide -A
 
 kubectl delete pod kube-flannel-ds-xnh7g -n kube-system
+
+kubectl get node 
+
+# kubelet 节点代理 ，指挥创建容器，管理pod 的生命周期
+# ps -ef|grep kubelet
+
+
+
+kubectl api-resources
+
+kubectl get pod --v=9
+
+kubectl apply -f ngx-pod.yml
+
+
+# 如何写yaml
+kubectl explain pod
+kubectl explain pod.metadata
+kubectl explain pod.spec
+kubectl explain pod.spec.containers
+# export out="--dry-run=client -o yaml"
+kubectl run ngx --image=nginx:alpine --dry-run=client -o yaml
+
+kubectl apply -f ngx-pod.yml
+kubectl delete -f ngx-pod.yml
+
+
+kubectl apply -f busy-pod.yml
+kubectl describe pod busy-pod
+
+
+kubectl logs ngx-pod
+
+# echo 'aaa' > a.txt
+kubectl cp a.txt ngx-pod:/tmp
+
+kubectl exec -it ngx-pod -- sh
+
