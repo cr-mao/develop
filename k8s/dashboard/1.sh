2717df5eb8a3c98baae7738f2405be560edbd922
cr-mao <crmao@qq.com>
2022-12-18 20:46:00 +0800

    官方dashboard

---
+++ k8s/dashboard/1.sh
@@ -0,0 +1,12 @@
+kubectl apply -f dashboard-adminuser.yaml
+kubectl apply -f recommended.yaml
+
+kubectl get service -A
+
+#访问测试
+# 每次访问都需要令牌
+kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
+
+
+# 访问地址
+# https://192.168.56.81:31605
